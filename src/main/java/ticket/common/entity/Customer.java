b24ab525e27599c1a9e584156193d44a976c1a60
Henry Hellbusch <hhellbusch@gmail.com>
2018-02-03 11:37:36 -0500

    comments describing Customer class and methods

--- src/main/java/ticket/common/entity/Customer.java
+++ src/main/java/ticket/common/entity/Customer.java
@@ -7,28 +7,49 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 
 /**
- * Entity that represents a customer
+ * Entity that represents a customer.
+ *
+  * @author Henry Hellbusch <hhellbusch@gmail.com>
+ * @since  2018.01.20
  */
 public class Customer implements Serializable
 {
-	private int id;
+	private Integer id;
 	private String email;
-	// private ArrayList<SeatHold> seatHolds;
 
-
-	public int getId()
+	/**
+	 * Returns the ID of the customer.  If the customer hasn't been
+	 * assigned an ID, it will be NULL.
+	 * @return ID of the customer
+	 */
+	public Integer getId()
 	{
 		return this.id;
 	}
+
+	/**
+	 * Sets an ID for the customer
+	 * @param id integer value to identify the customer by
+	 */
 	public void setId(int id)
 	{
 		this.id = id;
 	}
 
+	/**
+	 * Returns the email of the customer. Will be NULL if 
+	 * the email has not yet been set.
+	 * @return customer's email 
+	 */
 	public String getEmail()
 	{
 		return this.email;
 	}
+
+	/**
+	 * Sets the email address of the customer
+	 * @param email the email address to set for the customer
+	 */
 	public void setEmail(String email)
 	{
 		//email addresses are case insensitive - lets cast to it lowercase
